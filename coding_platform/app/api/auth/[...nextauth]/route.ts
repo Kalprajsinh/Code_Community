01f76c51f66abc3145db067a7dea8c6c389da736
Kalprajsinh <kalpraj51@gmail.com>
2024-10-20 11:26:30 +0530

    Auth

--- coding_platform/app/api/auth/[...nextauth]/route.ts
+++ coding_platform/app/api/auth/[...nextauth]/route.ts
@@ -1,31 +1,41 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import AppleProvider from 'next-auth/providers/apple'
-import FacebookProvider from 'next-auth/providers/facebook'
-import GithubProvider from "next-auth/providers/github"
+import AppleProvider from "next-auth/providers/apple";
+import FacebookProvider from "next-auth/providers/facebook";
+import GithubProvider from "next-auth/providers/github";
+import { NextResponse } from "next/server";
 
-const handler = NextAuth({
+const authOptions = {
   providers: [
     GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-      }),
-      AppleProvider({
-        clientId: process.env.APPLE_ID  ?? "",
-        clientSecret: process.env.APPLE_SECRET  ?? "",
-      }),
-      FacebookProvider({
-        clientId: process.env.FACEBOOK_ID   ?? "",
-        clientSecret: process.env.FACEBOOK_SECRET   ?? "",
-      }),
-      GithubProvider({
-        clientId: process.env.GITHUB_ID ?? "",
-        clientSecret: process.env.GITHUB_SECRET ?? "",
-      }),
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+    AppleProvider({
+      clientId: process.env.APPLE_ID ?? "",
+      clientSecret: process.env.APPLE_SECRET ?? "",
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID ?? "",
+      clientSecret: process.env.FACEBOOK_SECRET ?? "",
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID ?? "",
+      clientSecret: process.env.GITHUB_SECRET ?? "",
+    }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-})
+};
 
-export { handler as GET, handler as POST }
+// Define GET and POST handlers
+export async function GET(req: Request) {
+  const response = await NextAuth(authOptions);
+  return NextResponse.json(response);
+}
+
+export async function POST(req: Request) {
+  const response = await NextAuth(authOptions);
+  return NextResponse.json(response);
+}
 
 // export const GET = myfun;
